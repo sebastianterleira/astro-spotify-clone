8b9cb8947ddda04a4641ecdc9b44d373fbf5cd7f
sebastianterleira <seb.terleira1204@gmail.com>
2023-11-13 18:35:31 -0500

    del: Elimine la integracion de TailwindCSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [react()],
   output: "server",
   adapter: vercel()
 });
