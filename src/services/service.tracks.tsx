d4dd404da7e8bfbb66574b542fe7de23eca39674
sebastianterleira <seb.terleira1204@gmail.com>
2023-12-03 19:50:33 -0500

    add: Agregue el servicio para traer la data de los Tracks(canciones)

---
+++ src/services/service.tracks.tsx
@@ -0,0 +1,28 @@
+import { getPlaylistsData } from "./service.playlist";
+
+export const getTracks = async (token: string) => {
+
+  const { items } = await getPlaylistsData(token)
+  const tracks = Promise.all(items.map(async (playlist) =>  {
+    try {
+      const response = await fetch(playlist.tracks.href, {
+        headers: {
+          Authorization: "Bearer " + token,
+          "Content-Type": "application/json",
+        },
+      });
+  
+      if (!response.ok) {
+        throw new Error("Error al obtener las listas de canciones");
+      }
+  
+      const data = await response.json();
+      return data.items
+      
+    } catch (error) {
+      console.error("Error:", error.message);
+    }
+  }))
+  return tracks
+};
+
