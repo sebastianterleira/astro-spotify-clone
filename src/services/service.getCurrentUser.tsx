acfb7ea7ccf1618607b6ececc489bfd620337a50
sebastianterleira <seb.terleira1204@gmail.com>
2023-12-06 21:27:26 -0500

    add: Agregue el servicio para traer la informacion del Usuario actual

---
+++ src/services/service.getCurrentUser.tsx
@@ -0,0 +1,20 @@
+export const getCurrentUser = async (token: string) => {
+  const url = "https://api.spotify.com/v1/me";
+  try {
+    const response = await fetch(url, {
+      headers: {
+        Authorization: "Bearer " + token,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Error al obtener el usuario actual");
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+};
