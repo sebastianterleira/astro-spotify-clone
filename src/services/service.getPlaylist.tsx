8d737ef7ce671db2fd071c1809415c0721ce7352
sebastianterleira <seb.terleira1204@gmail.com>
2023-12-07 12:57:51 -0500

    Agregue el servicio para traer un los items completos de un playlist

---
+++ src/services/service.getPlaylist.tsx
@@ -0,0 +1,22 @@
+import type { ItemPlaylistAPIResponse } from "../types/item-playlist.types";
+
+export const getCurrentUser = async (token: string, playlist_id: string) => {
+  const url = `https://api.spotify.com/v1/playlists/${playlist_id}/tracks`;
+  try {
+    const response = await fetch(url, {
+      headers: {
+        Authorization: "Bearer " + token,
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Error al obtener el playlist");
+    }
+
+    const data = await response.json() as ItemPlaylistAPIResponse;
+    return data;
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+};
