a0ca4eabbfff50aff69967b4c4fde7bf75fa5596
sebastianterleira <seb.terleira1204@gmail.com>
2023-12-06 21:38:33 -0500

    add: Agregue los typos faltantes para los tracks

--- src/lib/playlists.type.ts
+++ src/lib/playlists.type.ts
@@ -23,6 +23,7 @@ export interface Item {
   tracks:        Tracks;
   type:          ItemType;
   uri:           string;
+  arrTrack:      arrTrack[];
 }
 
 export interface ExternalUrls {
@@ -55,4 +56,108 @@ export interface Tracks {
 
 export enum ItemType {
   Playlist = "playlist",
+}
+
+// arrTrack
+
+export interface arrTrack {
+  added_at:        Date;
+  added_by:        AddedBy;
+  is_local:        boolean;
+  primary_color:   null;
+  track:           Track;
+  video_thumbnail: VideoThumbnail;
+}
+
+export interface AddedBy {
+  external_urls: ExternalUrls;
+  href:          string;
+  id:            ID;
+  type:          AddedByType;
+  uri:           URI;
+  name?:         Name;
+}
+
+export enum ID {
+  The1OmO0LAKuNHuN7AIFbuJUP = "1OmO0lAKuNHuN7AIFbuJUP",
+  The1WGIhYkKWSq4YACTTkCkSX = "1wGIhYkKWSq4yACtTkCkSX",
+  The2PvyE8W9RWESQxkyAWZqgY = "2pvyE8W9RWESQxkyAWZqgY",
+  V24Ee4Tqnb7X4Fv1Xtd1R6Lni = "v24ee4tqnb7x4fv1xtd1r6lni",
+}
+
+export enum AddedByType {
+  Artist = "artist",
+  User = "user",
+}
+
+export enum URI {
+  SpotifyArtist1OmO0LAKuNHuN7AIFbuJUP = "spotify:artist:1OmO0lAKuNHuN7AIFbuJUP",
+  SpotifyArtist1WGIhYkKWSq4YACTTkCkSX = "spotify:artist:1wGIhYkKWSq4yACtTkCkSX",
+  SpotifyArtist2PvyE8W9RWESQxkyAWZqgY = "spotify:artist:2pvyE8W9RWESQxkyAWZqgY",
+  SpotifyUserV24Ee4Tqnb7X4Fv1Xtd1R6Lni = "spotify:user:v24ee4tqnb7x4fv1xtd1r6lni",
+}
+
+export enum Name {
+  Apache = "Apache",
+  Canserbero = "Canserbero",
+  NicoJP = "NicoJP",
+}
+
+export interface Track {
+  album:             Album;
+  artists:           AddedBy[];
+  available_markets: string[];
+  disc_number:       number;
+  duration_ms:       number;
+  episode:           boolean;
+  explicit:          boolean;
+  external_ids:      ExternalIDS;
+  external_urls:     ExternalUrls;
+  href:              string;
+  id:                string;
+  is_local:          boolean;
+  name:              string;
+  popularity:        number;
+  preview_url:       null | string;
+  track:             boolean;
+  track_number:      number;
+  type:              TrackType;
+  uri:               string;
+}
+
+export interface ExternalIDS {
+  isrc: string;
+}
+
+export enum TrackType {
+  Track = "track",
+}
+
+export interface VideoThumbnail {
+  url: null;
+}
+
+export interface Album {
+  album_type:             AlbumTypeEnum;
+  artists:                AddedBy[];
+  available_markets:      string[];
+  external_urls:          ExternalUrls;
+  href:                   string;
+  id:                     string;
+  images:                 Image[];
+  name:                   string;
+  release_date:           string;
+  release_date_precision: ReleaseDatePrecision;
+  total_tracks:           number;
+  type:                   AlbumTypeEnum;
+  uri:                    string;
+}
+
+export enum AlbumTypeEnum {
+  Album = "album",
+}
+
+export enum ReleaseDatePrecision {
+  Day = "day",
+  Year = "year",
 }
