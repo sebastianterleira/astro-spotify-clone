1beb1a81fa4ad7c543a3efd45bb6d436d613b197
sebastianterleira <seb.terleira1204@gmail.com>
2023-12-23 18:45:26 -0500

    fix: Solucione el nombramiento de Classes css

--- src/components/supcomponents/PlaylistTracks.jsx
+++ src/components/supcomponents/PlaylistTracks.jsx
@@ -16,16 +16,16 @@ export default function ListTracks({ playlist }) {
         playlist?.tracks?.items.map((track, index) => (
           <div
             onClick={() => handleClick(track.track.id)}
-            class={styles.card}
+            className={styles.card}
             key={index}
           >
             <div style={{ position: "relative", width: "1rem" }}>
               <p
-                class={`${styles.card__numberTrack} ${styles.position__trackNum}`}
+                className={`${styles.card__numberTrack} ${styles.position__trackNum}`}
               >
                 {trackNum[index]}
               </p>
-              <div class={styles.icon__play}>
+              <div className={styles.icon__play}>
                 <svg
                   height="21"
                   width="24"
@@ -36,21 +36,21 @@ export default function ListTracks({ playlist }) {
                 </svg>
               </div>
             </div>
-            <div class={styles.card__columnTitle}>
-              <picture class={styles.content__imgTrack}>
+            <div className={styles.card__columnTitle}>
+              <picture className={styles.content__imgTrack}>
                 <img
                   src={track.track.album.images[2].url}
                   height={40}
                   width={40}
-                  class={styles.img__track}
+                  className={styles.img__track}
                   alt={`Cover del album de la cancion ${track.track.name}`}
                 />
               </picture>
-              <div class={styles.content__titles}>
-                <p class={`${styles.track__tile} ${styles.hidden__text}`}>
+              <div className={styles.content__titles}>
+                <p className={`${styles.track__tile} ${styles.hidden__text}`}>
                   {track.track.name}
                 </p>
-                <p class={`${styles.track__artist} ${styles.hidden__text}`}>
+                <p className={`${styles.track__artist} ${styles.hidden__text}`}>
                   {track.track.artists[0].name +
                     `${
                       track.track?.artists[1]?.name === undefined
@@ -60,23 +60,23 @@ export default function ListTracks({ playlist }) {
                 </p>
               </div>
             </div>
-            <div class={styles.card__columnAlbum}>
-              <div class={styles.content__album}>
-                <p class={`${styles.album__name} ${styles.hidden__text}`}>
+            <div className={styles.card__columnAlbum}>
+              <div className={styles.content__album}>
+                <p className={`${styles.album__name} ${styles.hidden__text}`}>
                   {track.track.album.name}
                 </p>
               </div>
             </div>
-            <div class={styles.card__columnAdded}>
-              <div class={styles.content__added}>
-                <p class={`${styles.album__name} ${styles.hidden__text}`}>
+            <div className={styles.card__columnAdded}>
+              <div className={styles.content__added}>
+                <p className={`${styles.album__name} ${styles.hidden__text}`}>
                   {convertDate(track.added_at)}
                 </p>
               </div>
             </div>
             <div style={{ position: "relative", marginLeft: "2rem" }}>
               <p
-                class={`${styles.album__name} ${styles.hidden__text} ${styles.position_trackDuration}`}
+                className={`${styles.album__name} ${styles.hidden__text} ${styles.position_trackDuration}`}
               >
                 {msToMin(track.track.duration_ms)}
               </p>
@@ -84,9 +84,9 @@ export default function ListTracks({ playlist }) {
           </div>
         ))
       ) : (
-        <section class={styles.no__tracks}>
-          <div class={styles.line} />
-          <h2 class={styles["no__tracks--text"]}>
+        <section className={styles.no__tracks}>
+          <div className={styles.line} />
+          <h2 className={styles["no__tracks--text"]}>
             No hay nada en esta lista ...
           </h2>
         </section>
