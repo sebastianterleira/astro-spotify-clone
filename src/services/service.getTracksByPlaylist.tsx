7d9b1a54a712158fc690da1768245e2939bede4a
sebastianterleira <seb.terleira1204@gmail.com>
2023-12-30 11:06:36 -0500

    add: Agregue tipos a la respuesta de getTracksByPlaylist

--- src/services/service.getTracksByPlaylist.tsx
+++ src/services/service.getTracksByPlaylist.tsx
@@ -1,4 +1,4 @@
-import type { SpotifyTrackAPIResponse } from "../types/item-track.type";
+import type { TracksByPlaylistAPIResponse } from "src/types/tracks-by-playlist.types";
 
 export const getTracksByPlaylist = async (token: string, playlist_id: string) => {
   const url = `https://api.spotify.com/v1/playlists/${playlist_id}/tracks?offset=0&limit=10&locale=es-ES%2Ces%3Bq%3D0.9%2Cen%3Bq%3D0.8`;
@@ -15,7 +15,7 @@ export const getTracksByPlaylist = async (token: string, playlist_id: string) =>
       throw new Error("Error al obtener los tracks");
     }
 
-    const data = await response.json() as SpotifyTrackAPIResponse;
+    const data = await response.json() as TracksByPlaylistAPIResponse;
     return data;
   } catch (error) {
     console.error("Error:", error.message);
