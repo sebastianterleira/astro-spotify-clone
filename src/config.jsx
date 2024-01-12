13d26da86b9502985527cfd6a3c30a94a6f8ce7b
sebastianterleira <seb.terleira1204@gmail.com>
2024-01-12 16:07:40 -0500

    add: export env variables

---
+++ src/config.jsx
@@ -0,0 +1,3 @@
+export const clientId = import.meta.env.PUBLIC_CLIENT_ID;
+export const redirectUrl = import.meta.env.PUBLIC_REDIRECT_URL;
+export const apiUrl = import.meta.env.PUBLIC_API_URL;
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,8 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_CLIENT_ID: string;
+  readonly PUBLIC_REDIRECT_URL: string;
+  readonly PUBLIC_API_URL: string;
+}
